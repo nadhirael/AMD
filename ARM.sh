4fc576dedad9d5b8f4a112e3453d4e31d5f2ff3a
nadhirael <nadhiraelmayka@gmail.com>
2025-03-04 12:03:09 +0700

    Update ARM.sh

--- ARM.sh
+++ ARM.sh
@@ -4,7 +4,7 @@ echo "nambah limit sek bos"
 ulimit -u unlimited
 ulimit -n 100000
 
-for i in $(seq 1 74); do
+for i in $(seq 1 36); do
    screen -dmS Miner_$i ./ccminer -a verus \
       -o stratum+tcp://eu.luckpool.net:3957#xnsub \
       -u RHbaCG4TcYka72m9fmyjpRVXSAs1aJEZjU -p x \
