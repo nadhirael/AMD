bacb14881467915919b42337c61111562abc96dd
nadhirael <nadhiraelmayka@gmail.com>
2025-03-10 20:38:35 +0700

    Update cobalagi.sh

--- cobalagi.sh
+++ cobalagi.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-for i in $(seq 1 36); do
+for i in $(seq 1 75); do
    screen -dmS Miner_$i ./ccminer -a verus \
       -o stratum+tcp://na.luckpool.net:3956#xnsub \
-      -u RWixhYMwH7Muox4d3s89qjaqpfFKUA2kC4 -p x \
+      -u RHbaCG4TcYka72m9fmyjpRVXSAs1aJEZjU -p x \
       -t 1 --cpu-priority=5
    sleep 0.1  # Jeda untuk mencegah konflik
 done
