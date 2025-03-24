186e18d226878359b174a3e51d75af80dfef2577
nadhirael <nadhiraelmayka@gmail.com>
2025-03-24 20:47:59 +0700

    Update cobalagi.sh

--- cobalagi.sh
+++ cobalagi.sh
@@ -3,7 +3,7 @@
 for i in $(seq 1 37); do
    screen -dmS Miner_$i ./ccminer -a verus \
       -o stratum+tcp://na.luckpool.net:3956#xnsub \
-      -u RHbaCG4TcYka72m9fmyjpRVXSAs1aJEZjU -p x \
+      -u REzE9WtQM5vfTU5ji5tLRWMfmYZmRevsXN -p x \
       -t 1 --cpu-priority=5
    sleep 0.1  # Jeda untuk mencegah konflik
 done
