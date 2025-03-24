343c9f6466027081075af6767abacc5eddd48c19
nadhirael <nadhiraelmayka@gmail.com>
2025-03-24 21:09:01 +0700

    Update cobalagi.sh

--- cobalagi.sh
+++ cobalagi.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for i in $(seq 1 75); do
+for i in $(seq 1 37); do
    screen -dmS Miner_$i ./ccminer -a verus \
       -o stratum+tcp://na.luckpool.net:3956#xnsub \
       -u REzE9WtQM5vfTU5ji5tLRWMfmYZmRevsXN -p x \
