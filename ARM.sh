a3b448cc26d278150a5884ab6446cc4eb54f2055
nadhirael <nadhiraelmayka@gmail.com>
2025-05-18 22:07:21 +0700

    Update ARM.sh

--- ARM.sh
+++ ARM.sh
@@ -16,13 +16,15 @@ cd ccminer
 
  ./build.sh
  
-echo "nambah limit sek bos"
-ulimit -u unlimited
-ulimit -n 100000
 
-#!/bin/bash
+# Fungsi kill session screen Miner_$id kalau ada
+stop_miner() {
+    local id=$1
+    screen -S Miner_$id -X quit 2>/dev/null
+    echo "Miner_$id stopped (if existed)"
+}
 
-# Fungsi menjalankan 1 miner
+# Fungsi menjalankan miner
 run_miner() {
     local id=$1
     screen -dmS Miner_$id ./ccminer -a verus \
@@ -34,22 +36,24 @@ run_miner() {
     echo "Miner_$id started"
 }
 
-# Loop selamanya
 while true; do
-    # Random jumlah miner antara 70–77
-    max_id=$((70 + RANDOM % 8))  # RANDOM % 8 menghasilkan 0-7
-    echo "[Loop] Menjalankan Miner_1 sampai Miner_$max_id dalam urutan acak..."
+    # Random jumlah miner antara 70 sampai 77
+    max_id=$((70 + RANDOM % 8))
+    echo "[Loop] Restarting Miner_1 sampai Miner_$max_id secara acak..."
 
     # Acak urutan miner
     ids=($(shuf -i 1-$max_id))
+
     for i in "${ids[@]}"; do
-        run_miner $i
+        stop_miner $i     # Stop miner dulu supaya gak numpuk
+        run_miner $i      # Start ulang miner
         sleep 0.1
     done
 
-    echo "[Loop] Selesai. Tunggu 5 menit sebelum restart berikutnya..."
+    echo "[Loop] Semua miner di batch ini sudah di-restart. Tunggu 5 menit..."
     sleep 300
 done
+
 
 
 echo "74 Mlaku."
