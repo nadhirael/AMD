acc11cc844e67e18d8b8acbabeaa39b687563172
nadhirael <nadhiraelmayka@gmail.com>
2025-05-18 14:59:51 +0700

    Update cpulimit.sh

--- cpulimit.sh
+++ cpulimit.sh
@@ -4,7 +4,7 @@
 TARGET="ccminer"
 
 # Array limit CPU yang ingin digunakan secara bergantian (dalam %)
-LIMITS=(1450 1300 1250 1150 800)
+LIMITS=(1450 1350 1250 1150 900)
 
 # Fungsi untuk menghentikan cpulimit yang aktif
 kill_old_cpulimit() {
